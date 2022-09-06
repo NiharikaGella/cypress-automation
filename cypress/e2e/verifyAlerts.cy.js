73df2db360ddb1f734acb3866bb78f8ee5c491e9
Niharika Gella <niha.gella@gmail.com>
2022-09-06 23:40:24 +0530

    Code for verifying HomePage, RegisterPage, Alerts

---
+++ cypress/e2e/verifyAlerts.cy.js
@@ -0,0 +1,59 @@
+///<reference types = "cypress"/>
+it('verify home page',()=>{
+
+    cy.viewport(1280, 720);
+
+    //visiting the url
+    cy.visit('https://demo.automationtesting.in')
+
+    //navigating to Register
+    cy.get('#email').should('be.visible');
+    cy.get('#email').type("niha.gella@gmail.com");
+    cy.get('#enterimg').click();
+
+    //navigating to alerts page
+    cy.get(':nth-child(4) > .dropdown-menu > :nth-child(1) > a').click();
+    cy.get('.active > .analystic').scrollIntoView().should('be.visible');
+    cy.get(' #OKTab > .btn').scrollIntoView().should('be.visible').click();
+
+    //Click on 'Ok' alert box and verify the text
+    cy.on('window.alert',(str) =>
+    {
+    expect(str).to.equal(' I am an alert box!');
+    })
+
+    //Click on alert with ok and cancel > Press Ok and verify alert text
+    cy.get('#CancelTab > .btn').scrollIntoView().should('be.visible').click();
+    cy.on('window.confirm',(str) =>
+   {
+    expect(str).to.equal('Press a Button !');
+   }
+    )
+    cy.get('#demo').should('be.visible');
+
+
+     //Click on alert with ok and cancel > Press cancel and verify alert text
+    cy.get('#CancelTab > .btn').scrollIntoView().should('be.visible').click();
+    cy.on('window.confirm',function(confirmText)
+    { 
+        return false
+    }
+    )
+
+
+     //click on alert with text box > Enter Niharika and verify the alert text
+     cy.window().then(function(p){
+        //stubbing prompt window
+        cy.stub(p, "prompt").returns("Niharika");
+        // click on Click for JS Prompt button
+        cy.get('#Textbox > .btn').scrollIntoView().should('be.visible').click();
+        // verify application message on clicking on OK
+        cy.get('#demo1').contains('Hello Niharika How are you today');
+    
+
+   
+   
+    })
+   
+
+})
---
+++ cypress/e2e/verifyHomepage.cy.js
@@ -0,0 +1,49 @@
+///<reference types = "cypress"/>
+it('verify home page',()=>{
+
+    cy.viewport(1280, 720);
+
+    //visiting the url
+    cy.visit('https://demo.automationtesting.in')
+
+    //verifying if all buttons are present
+    cy.get('#btn1').should('be.visible');
+    cy.get('#btn2').should('be.visible');
+    cy.get('#logo').should('be.visible');
+
+    //verifying the input box, typing  a text and clicking enter
+    cy.get('#email').should('be.visible');
+    cy.get('#email').type("niha.gella@gmail.com");
+    cy.get('#enterimg').click();
+
+    //Entering all the mandatory details in register form, clicking on radio buttons, checking boxes and click on submit
+    cy.get(':nth-child(1) > :nth-child(2) > .form-control').scrollIntoView().should('be.visible').type("Niharika");
+    cy.get(':nth-child(1) > :nth-child(3) > .form-control').type("Gella");
+    cy.get('.col-md-8 > .form-control').type("langley"); 
+    cy.get('#eid > .form-control').type("niha.gella@gmail.com");
+    cy.get(':nth-child(4) > .col-md-4 > .form-control').type("7459539066");
+    cy.wait(5000);
+
+    //selecting the radio button
+    cy.get(':nth-child(5) > .col-md-4 > :nth-child(2) > input').check();
+
+    //checking the check box 
+    cy.get('#checkbox2').check();
+
+    //selecting from drop down 
+   cy.get('.select2-selection__arrow').click();
+    cy.get('.select2-search__field').type('India{enter}');
+   
+
+    //click on submit
+    cy.get('#submitbtn').click();
+    
+   
+
+
+    
+   
+    
+
+
+})
